ebaba8bbb2aa7684b185af773063212e9f3fe352
lletac <l.letac@3eing.ca>
2021-02-23 15:46:20 -0500

    correction renvoie de page, ajout de validation de fichier et de purge des fichiers ajout de sous dossier upload en cours

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, Response
+from werkzeug.utils import secure_filename
+from utils.File import validate_file_epow as validate, uploads_files, purger_upload
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 3072 * 3072
+app.config['UPLOAD_EXTENSIONS'] = ['.csv', '.xlsx', '.xls']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
 def index():
@@ -8,13 +15,29 @@ def index():
 
 @app.route('/eepower', methods=['GET','POST'])
 def eepower():
+    uploaded_files = uploads_files(app.config['UPLOAD_PATH'])
     if request.method == 'POST':
-        for uploaded_file in request.files.getlist('file'):
-            if uploaded_file.filename != '':
-                uploaded_file.save(uploaded_file.filename)
-        return redirect(url_for('eepower'))
+        #ajout de fichier pour analyse
+        if request.form['btn_id'] == 'soumettre_fichier':
+            for uploaded_file in request.files.getlist('file'):
+                filename = uploaded_file.filename
+                if filename != '':
+                    file_ext = os.path.splitext(filename)[1]
+                    #valide si l'extension des fichier est bonne
+                    if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                        abort(Response("Les fichiers reçus ne sont des fichiers .csv ou .xlsx"))
+                    uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+                    #valide en ouvrant les fichier si le contenue est bon
+                    if validate(os.path.join(app.config['UPLOAD_PATH'], 'eepower', filename)) != 0:
+                        abort(Response("Les fichiers reçus ne contiennent pas les informations nécessaires ou n'ont "
+                                       "pas le bon format"))
+            return  redirect(url_for('eepower'))
 
-    return render_template('easy_power.html')
+        elif request.form['btn_id'] == 'purger':
+            purger_upload(os.path.join(app.config['UPLOAD_PATH'], 'eepower'))
+            return redirect(url_for('eepower'))
+
+    return render_template('easy_power.html', uploaded_files=uploaded_files)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ utils/File.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+from shutil import rmtree
+
+
+def uploads_files(dir_name=r'./uploads'):
+    if os.path.exists(dir_name) and os.path.isdir(dir_name):
+        if not os.listdir(dir_name):
+            print("Directory is empty")
+            return []
+        else:
+            print("Directory is not empty")
+            return os.listdir(dir_name)
+    else:
+        print("Given directory doesn't exist")
+        return []
+
+
+def purger_upload(dir_name=r'./uploads'):
+    for file in uploads_files(dir_name):
+        rmtree(os.path.join(dir_name, file))
+
+def validate_file_epow(file):
+    col30 = set(['Bus kV', 'Sym Amps'])
+    col1 = set(["Bus kV","Sym Amps","X/R Ratio","Mult Factor","Asym Amps","Equip Type","Duty Amps"])
+    try:
+        df = pd.DataFrame(pd.read_csv(file, skiprows=1, index_col=0))
+    except:
+        try:
+            df = pd.DataFrame(pd.read_excel(file, skiprows=5, index_col=0, engine='openpyxl'))
+        except openpyxl.utils.exceptions.InvalidFileException as notXL:
+            return -1
+
+    if col1.issubset(df.columns.to_list()) or col30.issubset(df.columns.to_list()):
+        return 0
+    else:
+        return -1
+
+
+if __name__ == "__main__":
+    purger_upload(r'../uploads')
