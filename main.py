d0e683b91aa5e251dbed2ebba8b62147af87372f
lletac <l.letac@3eing.ca>
2022-11-10 15:39:17 -0500

    changement de main pour utiliser fstAPI et uvicorn en debug

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app.utils.db_dev_api import app as backend
 from app.app import app as frontend
 from fastapi.middleware.wsgi import WSGIMiddleware
+import uvicorn
 
 backend.mount("/", WSGIMiddleware(frontend), name='frontend')
+
+if __name__ == "__main__":
+    uvicorn.run(backend, host="localhost", port=5000)
 
